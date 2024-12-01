f9a6ead8d588f6c0e761907329eccc9133496f24
clomollo <oyoocliff471@gmail.com>
2024-12-01 14:46:27 +0300

    Simulating stack with Lists

---
+++ stackOps.py
@@ -0,0 +1,26 @@
+#Simulating Stack with Lists
+
+
+#Stack are constrained list
+#You push using list method append(adds new element to the end of the list)
+#You pop using list method pop with no arguments, which removes and returns the item at the end of the list
+
+
+stack=[]
+
+stack.append('red')
+print(stack)
+
+stack.append('Brown')
+stack.append('Kelly')
+print(stack)
+
+stack.pop()
+
+stack.pop()
+
+stack.pop()
+
+
+# stack.pop()
+print(stack)
